719dd46af52ad938a15a549c4a98478633a15ebf
Milin Khalas <milin.khalas@attsystemsgroup.com>
2024-11-08 01:35:59 +0530

    Created an angular application.

---
+++ creation-engineers-app/src/app/admin/admin.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './products/products.component';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProductsComponent
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
+export class AdminModule { }
